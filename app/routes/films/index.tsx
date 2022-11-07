b4e27d9373261faa47957958cb7a3c78693c8150
Zin Paing Soe <kuro@Zins-MacBook-Pro.local>
2022-11-08 00:20:17 +0630

    loader function

--- app/routes/films/index.tsx
+++ app/routes/films/index.tsx
@@ -1,9 +1,32 @@
 import { LinksFunction, LoaderFunction, MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Film, getFilms } from "~/api/films";
 
 /**
- * Server side: Function that will be called on the server before rendering to provide data to the route
+ * Server side:
+ * Function that will be called on the server before rendering to provide data to the route
  */
-export const loader: LoaderFunction = () => {};
+export const loader: LoaderFunction = async () => {
+  return getFilms();
+};
+
+// Client side
+export default function FilmsIndex() {
+  const films = useLoaderData<Film[]>();
+  return (
+    <div className="p-16 font-sans">
+      <h1 className="text-5xl font-bold text-center">Studio Ghibli Films</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {films.map((film) => (
+          <div className="hover:shadow-2xl hover:scale-105 hover:font-bold cursor-pointer">
+            <div>{film.title} </div>
+            <img src={film.image} alt={film.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export const meta: MetaFunction = () => ({
   title: "Films | Studio Ghibli",
@@ -16,7 +39,3 @@ export const meta: MetaFunction = () => ({
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: "styles" }];
 };
-
-export default function FilmsIndex() {
-  return <div>Films</div>;
-}
