a2c32fae7f1031d7684dd52e239ec608a24c5121
Zin Paing Soe <kuro@Zins-MacBook-Pro.local>
2022-11-08 00:30:57 +0630

    HTML form get

--- app/routes/films/index.tsx
+++ app/routes/films/index.tsx
@@ -6,8 +6,10 @@ import { Film, getFilms } from "~/api/films";
  * Server side:
  * Function that will be called on the server before rendering to provide data to the route
  */
-export const loader: LoaderFunction = async () => {
-  return getFilms();
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const title = url.searchParams.get("title");
+  return getFilms(title);
 };
 
 // Client side
@@ -16,6 +18,24 @@ export default function FilmsIndex() {
   return (
     <div className="p-16 font-sans">
       <h1 className="text-5xl font-bold text-center">Studio Ghibli Films</h1>
+
+      <form className="py-5">
+        <label className="font-bold">
+          Search{" "}
+          <input
+            type="text"
+            name="title"
+            placeholder="Type a title..."
+            className="border-2 rounded py-2 px-3"
+          />
+        </label>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2"
+        >
+          Search
+        </button>{" "}
+      </form>
       <div className="grid grid-cols-4 gap-4">
         {films.map((film) => (
           <div className="hover:shadow-2xl hover:scale-105 hover:font-bold cursor-pointer">
